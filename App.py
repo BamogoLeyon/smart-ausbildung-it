ba2726168fb94656e5c44553680dadfedac426aa
BamogoLeyon <leonbamogo91@gmail.com>
2026-03-05 00:38:07 +0000

    Add welcome function for SmartAusbildung application

---
+++ App.py
@@ -0,0 +1,11 @@
+def willkommen():
+    print("Willkommen bei SmartAusbildung!")
+    print("Diese Anwendung hilft bei der Orientierung für IT-Ausbildungen in Deutschland.")
+    print("")
+    print("Beliebte IT-Berufe:")
+    print("1. Fachinformatiker/in - Anwendungsentwicklung")
+    print("2. Fachinformatiker/in - Systemintegration")
+    print("3. IT-Systemelektroniker/in")
+
+if __name__ == "__main__":
+    willkommen()
