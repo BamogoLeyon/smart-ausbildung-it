1bccab002b740f742a53cf7a86d4c4fca123c462
Bamogo Leyon <leonbamogo91@.com>
2026-03-06 01:44:57 +0100

    fix deploy

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def home():
 
 @app.route("/berufe")
 def berufe():
-    return render_template("berufe.html")
+    return "<h1>IT Berufe Seite</h1>"
 
 if __name__ == "__main__":
-    app.run()  
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
